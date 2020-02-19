e193ec5d67c80590654dc07b252715a4eb3d8816
YM <thehousewhite@gmail.com>
2020-02-19 21:51:06 +0100

    commit on newmac

--- banana.py
+++
@@ -1,8 +0,0 @@
-
-
-def peel():
-    print(f"My name is {__name__}")
-    return "peel an banana"
-
-if __name__ is "__name__": # used when i want to prevent a block of code from running on an import
-    peel()
