0b4b6157803af8e00988222a2404c657948d9328
YM <thehousewhite@gmail.com>
2020-03-14 19:36:40 +0100

    get and post request

--- Flask/section4/app.py
+++ Flask/section4/app.py
@@ -5,14 +5,24 @@ app = Flask(__name__)
 
 api = Api(app)
 
+items = []
+
 # Resource is a class which allows GET POST UPDATE....)
-class Student(Resource):
+class Item(Resource):
     # define the method that the resoure is going to get
     def get(self,name):
-        return {"student": name}
+        for item in items:
+            if item["name"] == name:
+                return item #return 200 as status cod
+        return {"item":None}, 404
+
+    def post(self,name):
+        item = {"name": name, "price": 66.66}
+        items.append(item)
+        return item, 201 # return 201 for creating successfully
 
 # api works with resources (likely DB tables or class)
-api.add_resource(Student, "/student/<string:name>") # http://127.0.0.1:5000/student/nick
+api.add_resource(Item, "/item/<string:name>") # http://127.0.0.1:5000/item/<name>
 
 
 app.run(port=5000)
